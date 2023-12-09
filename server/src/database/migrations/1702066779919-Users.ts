b69a6adfff1024d4d10918118820592c77bc091d
maycon silva <maycon8609@gmail.com>
2023-12-08 22:48:20 -0300

    feat: added migrate to create user table

---
+++ server/src/database/migrations/1702066779919-Users.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm"
+
+export class Users1702066779919 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid'
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'cpf',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users')
+  }
+}
