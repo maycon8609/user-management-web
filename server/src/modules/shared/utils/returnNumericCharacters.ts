9aa80a3d4eb902a6bf70e1f97c87f5eb07cc516a
maycon silva <maycon8609@gmail.com>
2024-01-09 13:55:39 -0300

    feat: init the project

--- server/src/modules/shared/utils/returnNumericCharacters.ts
+++
@@ -1,3 +0,0 @@
-export function returnNumericCharacters(value: string): string {
-  return value.replace(/\D/g, '');
-}
--- server/src/modules/users/application/types.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@users/domain/User"
-
-export interface IUserService {
-  createUser(user: User): Promise<User>
-  findUserByCpf(cpf: string): Promise<User>
-  getAllUsers(): Promise<User[] | []>
-  updateUser(user: User): Promise<User>
-  deleteUser(cpf: string): Promise<void>
-}
